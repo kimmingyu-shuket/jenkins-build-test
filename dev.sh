a6a72bdaad5e734419c7537df171636dba0b302b
Kimmingyu-shuket <mg.kim@shuket.company>
2022-12-08 13:08:18 +0900

    cicd test

--- dev.sh
+++ dev.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "node_modules remove."
-rm -r node_modules
+rm -rf node_modules package-lock.json
 
 echo "npm install start."
-# npm install
+npm install --save
 echo "npm install success."
 
 echo "npm build start."
-# npm run build
+npm run build
 echo "npm build end"
 ls -al
